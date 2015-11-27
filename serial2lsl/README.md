2e663022a0c819a479a523ef16b5a61c68d69cf2
jfrey <nomhad@gmail.com>
2015-11-26 19:47:22 -0500

    update dep + adjust rate for print

--- serial2lsl/README.md
+++ serial2lsl/README.md
@@ -1,3 +1,5 @@
 Simple script that will stream (string) values from serial port to LSL.
 
-pip install pylsl
+Requires LSL: https://github.com/sccn/labstreaminglayer/
+
+Either install using `pip install pylsl` or download lib at: ftp://sccn.ucsd.edu/pub/software/LSL/
--- serial2lsl/serial2lsl.py
+++ serial2lsl/serial2lsl.py
@@ -1,20 +1,41 @@
 # Caution, quick'n dirty!
 
 import serial, time
+from pylsl import StreamInfo, StreamOutlet
 
+# parameters for com port
+# TODO: from command line
 port = '/dev/ttyACM0'
 baudrate = 115200
+# 2ms pause in arduino firmware: 500Hz
+samplingrate = 500
+# rate for spamming stdout
+displaySamplingRate = 10
 
 ser = serial.Serial(port, baudrate)
-time.sleep(1)
 
+# create LSL StreamOutlet
+info = StreamInfo('breath','breath',1,samplingrate,'float32','conphyturebreathing1337')
+outlet = StreamOutlet(info)
+
+# some time for init?
+#time.sleep(0.5)
+
+n=0
 while True:
-  line = ""
+  line = ser.readline()
+  value = 0
+  # convert string to float (remove trailing line break at the same time...)
   try:
-    line = ser.read(1)
+    value = float(line)
   except:
-    print "error"
-  print line
-  time.sleep(0.002)
+    # silently discard conversion problem
+    pass
+  #TODO: won't work if rates not dividers
+  if n%(samplingrate/displaySamplingRate) == 0:
+    print value
+  outlet.push_sample([value])
+  n+=1
+  #time.sleep(0.002)
   
 ser.close()
